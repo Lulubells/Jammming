dd642e7fd562a5c2bb0310e369285dd0eb968a7e
Lulubell <34559423+Lulubells@users.noreply.github.com>
2018-12-07 00:32:20 -0600

    Update App.js

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
 import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
+import Spotify from '../../util/Spotify';
 
 class App extends React.Component {
 constructor(props) {
@@ -42,30 +43,38 @@ updatePlaylistName(name) {
 
 savePlaylist() {
   const trackUris = this.state.playlistTracks.map(track => track.uri);
+  Spotify.savePlaylist(this.state.playlistName, trackUris).then(() => {
+    this.setState({
+      playlistName: 'New Playlist',
+      playlistTracks: []
+    });
+  });
 }
 
 search(term) {
-console.log(term);
+  Spotify.search(term).then(searchResults => {
+    this.setState({searchResults: searchResults});
+  });
 }
 
- render() {
-    return (
-      <div>
-        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+render() {
+  return (
+    <div>
+      <h1>Ja<span className="highlight">mmm</span>ing</h1>
       <div className="App">
-        <SearchBar onSearch={this.search}/>
-      <div className="App-playlist">
-        <SearchResults searchResults={this.state.searchResults}
-          onAdd={this.addTrack} />
-        <Playlist playlistTracks={this.state.playlistTracks}
-           playlistTracks={this.state.playlistTracks} 
-           onRemove={this.removeTrack}
-           onNameChange={this.updatePlaylistName} />
+        <SearchBar onSearch={this.search} />
+        <div className="App-playlist">
+          <SearchResults searchResults={this.state.searchResults}
+                         onAdd={this.addTrack} />
+          <Playlist playlistTracks={this.state.playlistTracks}
+                    onNameChange={this.updatePlaylistName}
+                    onRemove={this.removeTrack}
+                    onSave={this.savePlaylist} />
         </div>
       </div>
     </div>
-    );
-  }
+  );
+}
 }
 
 export default App;
